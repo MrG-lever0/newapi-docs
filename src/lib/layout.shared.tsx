2d1d47357295fc30483e9dc59405edcbcceb07f7
MrG-lever0 <1956972057@qq.com>
2026-03-19 14:40:37 +0800

    Initial commit

---
+++ src/lib/layout.shared.tsx
@@ -0,0 +1,34 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { i18n } from '@/lib/i18n';
+import Image from 'next/image';
+import type { LinkItemType } from 'fumadocs-ui/layouts/docs';
+
+export const linkItems: LinkItemType[] = [];
+
+export const logo = (
+  <Image
+    alt="New API"
+    src="/assets/newapi.svg"
+    width={20}
+    height={20}
+    className="size-5"
+    priority
+    unoptimized
+  />
+);
+
+export function baseOptions(locale: string): BaseLayoutProps {
+  return {
+    i18n,
+    nav: {
+      title: (
+        <>
+          {logo}
+          <span className="font-medium in-[header]:text-[15px] [.uwu_&]:hidden">
+            New API
+          </span>
+        </>
+      ),
+    },
+  };
+}
